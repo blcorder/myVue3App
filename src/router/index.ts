774bdfba493b70e846b175ea2a420996dda5001f
hbw_ <hbw253289>
2023-09-07 20:24:50 +0800

    '接口定义'

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,31 +1,31 @@
-import { createRouter, createWebHistory} from 'vue-router'
-import  type { RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/home.vue'
 
-const routes :Array<RouteRecordRaw> = [
+const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect:'/home',
+    redirect: '/home',
   },
   {
     path: '/home',
     name: 'home',
     component: HomeView,
-    children:[
+    children: [
       {
         path: '/Menu1',
         name: 'Menu1',
-        component: ()=> import('../views/components/Menu1.vue')
+        component: () => import('../views/components/Menu1.vue')
       },
       {
         path: '/Menu2',
         name: 'Menu2',
-        component: ()=> import('../views/components/Menu2.vue')
+        component: () => import('../views/components/Menu2.vue')
       },
       {
         path: '/Menu3',
         name: 'Menu3',
-        component: ()=> import('../views/components/Menu3.vue')
+        component: () => import('../views/components/Menu3.vue')
       }
     ]
   }
--- src/views/Nav.vue
+++ src/views/Nav.vue
@@ -1,42 +1,38 @@
 <script setup lang="ts">
-    import {
+import {
   Document,
   Menu as IconMenu,
   Location,
   Setting,
 } from '@element-plus/icons-vue'
-// const handleOpen = (key: string, keyPath: string[]) => {
-//   console.log(key, keyPath)
-// }
-// const handleClose = (key: string, keyPath: string[]) => {
-//   console.log(key, keyPath)
-// }
+import { reactive } from 'vue'
+
+interface Imenus {
+  name: string,
+  url: string,
+  icon?: any
+}
+const items: Imenus[] = [
+  { name: "菜单一", url: '/Menu1', icon: Setting },
+  { name: "菜单二", url: '/Menu2', icon: Document },
+  { name: "菜单三", url: '/Menu3', icon: Location }
+]
+const menus = reactive(items)
 </script>
 
 
 <template>
   <div class="nav-aside">
-    <el-menu
-        default-active="2"
-        class="el-menu-vertical-demo"
-        router
-      >
-      <el-menu-item index="/Menu1">
-                <el-icon><Setting /></el-icon>
-                菜单一
-            </el-menu-item>
-            <el-menu-item index="/Menu2">
-                <el-icon><Document /></el-icon>
-                菜单二
-            </el-menu-item>
-            <el-menu-item index="/Menu3">
-                <el-icon><Location /></el-icon>
-                菜单三
-            </el-menu-item>
-      </el-menu>
+    <el-menu default-active="2" class="el-menu-vertical-demo" router>
+      <el-menu-item v-for="item in menus" :index="item.url">
+        <el-icon>
+          <component :is="item.icon" />
+        </el-icon>
+        {{ item.name }}
+      </el-menu-item>
+    </el-menu>
   </div>
 </template>
 
 
-<style scoped lang="less">
-</style>
+<style scoped lang="less"></style>
